8c97ba7f1faf17e1c89045499f956540712608f7
GabrielCPinto <gabrielcostap112@gmail.com>
2023-12-19 11:12:34 -0300

    fim

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # conversor-de-moedas
+
+Conversor de moedas que converte entre USD, CAD, EUR, BRL, JPY. A chave da API se encontra no arquivo key.py, com o nome de API_KEY.
---
+++ currency.py
@@ -0,0 +1,29 @@
+import requests
+
+from key import API_KEY
+
+BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
+
+CURRENCIES = ['USD', 'CAD', 'EUR', 'BRL', 'JPY']
+
+def convert_currency(base):
+    currencies = ','.join(CURRENCIES)
+    url = f'{BASE_URL}&base_currency={base}&currencies={currencies}'
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data['data']
+    except:
+        print('Invalid currency.')
+        return None
+
+def main():
+    base = input('Base currency: ').upper()
+    
+    data = convert_currency(base)
+    del data[base]
+    for ticker, value in data.items():
+        print(f'{ticker}: {value}')
+
+if __name__ == '__main__':
+    main()
